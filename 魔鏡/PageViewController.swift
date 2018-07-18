8bb5662e6e27708ca5069df45f65f8356cfa3d11
Jony Wang <jony@FalconedeMacBook-Air.local>
2018-07-18 14:16:41 +0800

    Finish soon

--- 魔鏡/PageViewController.swift
+++ 魔鏡/PageViewController.swift
@@ -1,11 +1,3 @@
-//
-//  PageViewController.swift
-//  魔鏡
-//
-//  Created by Jony Wang on 2018/7/3.
-//  Copyright © 2018年 Jony. All rights reserved.
-//
-
 import UIKit
 
 class PageViewController: UIPageViewController, UIPageViewControllerDataSource {
@@ -26,38 +18,24 @@ class PageViewController: UIPageViewController, UIPageViewControllerDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        // vc1 ~ vc6 為介紹頁面 ， vc7 為開始介面。
+        // vc3 ~ vc6 為介紹頁面
         
         view.backgroundColor = UIColor.white
-//    
-//        let imageView = UIImageView(image: UIImage(named: "note"))
-//        imageView.frame = view.bounds
-//        view.addSubview(imageView)
-//        
         
-        //let vc2 = storyboard?.instantiateViewController(withIdentifier: "vc2")
         let vc3 = storyboard?.instantiateViewController(withIdentifier: "vc3")
         let vc4 = storyboard?.instantiateViewController(withIdentifier: "vc4")
         let vc5 = storyboard?.instantiateViewController(withIdentifier: "vc5")
         let vc6 = storyboard?.instantiateViewController(withIdentifier: "vc6")
-        //let vc7 = storyboard?.instantiateViewController(withIdentifier: "vc7")
-        
-        //list.append(vc2!)
+       
         list.append(vc3!)
         list.append(vc4!)
         list.append(vc5!)
         list.append(vc6!)
-        //list.append(vc7!)
-        
-        
-        
         
         setViewControllers([list[0]], direction: .forward, animated: true, completion: nil)
         
         dataSource = self
-//        app.page_control.translatesAutoresizingMaskIntoConstraints = false
-//        app.page_control.topAnchor.constraint(equalTo: (vc2?.view.bottomAnchor)!, constant: -100).isActive = true
+
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
@@ -81,21 +59,5 @@ class PageViewController: UIPageViewController, UIPageViewControllerDataSource {
     func presentationIndex(for pageViewController: UIPageViewController) -> Int {
         return 0
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
